337f17af3aaf34813ab60163900bede82e9559a7
Bikrant <84465031+bikrantlabs@users.noreply.github.com>
2024-08-26 00:47:21 +0545

    feat(ui): add icon button in sidebar

--- include/views/SidebarView.hpp
+++ include/views/SidebarView.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "gtkmm/grid.h"
+#include "icon_button.hpp"
 #include <gtkmm.h>
 
 class SidebarView : public Gtk::Box {
@@ -9,4 +11,6 @@ public:
 
 private:
   // Additional UI elements for the sidebar can be added here
+  Gtk::Grid primaryActionButtonGrid;
+  IconButton addCategoryBtn;
 };
--- src/views/SidebarView.cpp
+++ src/views/SidebarView.cpp
@@ -1,8 +1,17 @@
 #include "SidebarView.hpp"
-
-SidebarView::SidebarView() : Gtk::Box(Gtk::Orientation::VERTICAL) {
+#include <iostream>
+void onIconButtonClick() { std::cout << "Clicked~"; }
+SidebarView::SidebarView()
+    : Gtk::Box(Gtk::Orientation::VERTICAL),
+      addCategoryBtn("../folder-plus.png", onIconButtonClick) {
 
   // Set background color for visual differentiation
+  append(primaryActionButtonGrid);
+  primaryActionButtonGrid.attach(addCategoryBtn, 0, 0, 1, 1);
+  primaryActionButtonGrid.set_column_homogeneous(
+      false); // Make all columns have the same width
+  primaryActionButtonGrid.set_row_homogeneous(
+      false); // Rows can have different heights
   set_name("sidebar");
   // Additional initialization code can go here
 }
